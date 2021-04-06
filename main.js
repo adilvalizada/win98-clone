43051bcf56d34e9cef519c5fa3b576978e6a1f34
adilvelizade0 <71330310+adilvelizade0@users.noreply.github.com>
2021-04-06 11:08:01 +0400

    version: 0.0.7

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 "use strict";
-let openAlert = true;
 
-// * Alert Box
+const startBar = document.querySelector(".start-bar").getBoundingClientRect()
+  .height;
+
+// * Alert Module
 //////////////////////////////////////////////////////////////////////////////////////////////
+let openAlert = true;
 setTimeout(() => {
   document.querySelector(".open-alert").style.display = "inline-block";
 }, 1000);
@@ -62,7 +65,8 @@ document.querySelector(".maximize-btn-img").addEventListener("click", () => {
   if (openImg) {
     document.querySelector(".open-img").style.width = "1366px";
     document.querySelector(".open-img").style.overflow = "auto";
-    document.querySelector(".open-img").style.height = "605px";
+    document.querySelector(".open-img").style.height =
+      document.documentElement.clientHeight - startBar + "px";
     document.querySelector(".open-img").style.top = "0";
     document.querySelector(".open-img").style.left = "0";
     document.querySelector(".open-img").style.position = "fixed";
@@ -110,12 +114,12 @@ document.querySelector(".secretfile").addEventListener("dblclick", () => {
   if (isClose) {
     document.querySelector(".open-img").style.opacity = "1";
     document.querySelector(".open-img").style.display = "block";
-    document.querySelector(".open-img").style.width = "1366px";
+    document.querySelector(".open-img").style.width = "100%";
     document.querySelector(".open-img").style.overflowY = "auto";
     document.querySelector(".open-img").style.overflowX = "hidden";
-    (document.querySelector(".open-img").style.height =
-      document.querySelector(".start-bar").left + window.scrollX) + "px",
-      (document.querySelector(".open-img").style.top = "0");
+    document.querySelector(".open-img").style.height =
+      document.documentElement.clientHeight - startBar + "px";
+    document.querySelector(".open-img").style.top = "0";
     document.querySelector(".open-img").style.left = "0";
     document.querySelector(".open-img").style.position = "fixed";
     document.querySelector(".img-bar-window").style.display = "inline-block";
@@ -194,3 +198,115 @@ document.querySelector(".line-up-icons").addEventListener("click", () => {
     el.style.right = 0;
   });
 });
+
+// * Properties Module
+let minimizePro = true;
+let isClosePro = true;
+document.querySelector(".properties").addEventListener("click", () => {
+  document.querySelector(".sys-pro").style.opacity = "1";
+  document.querySelector(".sys-pro").style.display = "block";
+  document.querySelector(".pro-bar-window").style.display = "block";
+});
+
+document.querySelector(".close-btn-pro").addEventListener("click", () => {
+  document.querySelector(".sys-pro").style.opacity = "0";
+  isClosePro = !isClosePro;
+  setTimeout(() => {
+    document.querySelector(".sys-pro").style.display = "none";
+    document.querySelector(".pro-bar-window").style.display = "none";
+  }, 300);
+});
+
+document.querySelector(".minimize-btn-pro").addEventListener("click", () => {
+  document.querySelector(".sys-pro").style.display = "none";
+  minimizePro = !minimizePro;
+});
+
+document.querySelector(".pro-bar-window").addEventListener("click", () => {
+  if (!minimizePro) {
+    document.querySelector(".sys-pro").style.display = "block";
+    minimizePro = !minimizePro;
+  } else {
+    document.querySelector(".sys-pro").style.display = "none";
+    minimizePro = !minimizePro;
+  }
+});
+
+document.querySelector(".pro-ok-btn").addEventListener("click", () => {
+  document.querySelector(".sys-pro").style.opacity = "0";
+  document.querySelector(".sys-pro").style.display = "none";
+  document.querySelector(".pro-bar-window").style.display = "none";
+
+  isClosePro = !isClosePro;
+});
+document.querySelector(".pro-cancel-btn").addEventListener("click", () => {
+  document.querySelector(".sys-pro").style.opacity = "0";
+
+  document.querySelector(".sys-pro").style.display = "none";
+  document.querySelector(".pro-bar-window").style.display = "none";
+
+  isClosePro = !isClosePro;
+});
+
+// *  My Computer Module
+let openMyComp = true;
+let minimizeMyComp = true;
+let isCloseMyComp = true;
+
+document.querySelector(".myComputer").addEventListener("dblclick", () => {
+  if (isCloseMyComp) {
+    document.querySelector(".my-computer").style.opacity = "1";
+
+    document.querySelector(".my-computer").style.display = "block";
+    document.querySelector(".myComp-bar-window").style.display = "block";
+    isCloseMyComp = !isCloseMyComp;
+  }
+});
+
+document.querySelector(".close-btn-myComp").addEventListener("click", () => {
+  document.querySelector(".my-computer").style.opacity = "0";
+  isCloseMyComp = !isCloseMyComp;
+  setTimeout(() => {
+    document.querySelector(".my-computer").style.display = "none";
+    document.querySelector(".myComp-bar-window").style.display = "none";
+  }, 300);
+});
+
+document.querySelector(".minimize-btn-myComp").addEventListener("click", () => {
+  document.querySelector(".my-computer").style.display = "none";
+  minimizeMyComp = !minimizeMyComp;
+});
+
+document.querySelector(".myComp-bar-window").addEventListener("click", () => {
+  if (!minimizeMyComp) {
+    document.querySelector(".my-computer").style.display = "block";
+    minimizeMyComp = !minimizeMyComp;
+  } else {
+    document.querySelector(".my-computer").style.display = "none";
+    minimizeMyComp = !minimizeMyComp;
+  }
+});
+
+document
+  .querySelector(".maximize-btn-myComp")
+  .addEventListener("click", (event) => {
+    if (openMyComp) {
+      document.querySelector(".my-computer").style.width = "100%";
+      document.querySelector(".my-computer").style.overflow = "auto";
+      document.querySelector(".my-computer").style.height =
+        document.documentElement.clientHeight - startBar + "px";
+      document.querySelector(".my-computer").style.top = "0";
+      document.querySelector(".my-computer").style.left = "0";
+      document.querySelector(".my-computer").style.position = "fixed";
+      document.querySelector(".my-cmp-main").style.height = "450px";
+      openMyComp = !openMyComp;
+    } else {
+      document.querySelector(".my-computer").style.width = "800px";
+      document.querySelector(".my-computer").style.overflow = "none";
+      document.querySelector(".my-computer").style.height = "auto";
+      document.querySelector(".my-computer").style.top = "15%";
+      document.querySelector(".my-computer").style.left = "15%";
+      document.querySelector(".my-computer").style.position = "absolute";
+      openMyComp = !openMyComp;
+    }
+  });
